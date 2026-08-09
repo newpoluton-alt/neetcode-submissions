3a2a308d1347de9e76341b34d45b1458bf0c8332
Aziret Ashyrbai uulu <newpoluton@gmail.com>
2026-08-09 12:15:15 +0200

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = s.lower().replace(" ","")
+        for l in "!?,.':;":
+            strs = strs.replace(l,"")
+
+        if strs == strs[::-1]:
+            return True
+        else:
+            return False
+        
