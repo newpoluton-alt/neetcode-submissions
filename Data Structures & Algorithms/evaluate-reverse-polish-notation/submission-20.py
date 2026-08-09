d998ea576510208320169a49df641fdbe6321b92
Aziret Ashyrbai uulu <newpoluton@gmail.com>
2026-08-09 13:52:18 +0200

    Add: evaluate-reverse-polish-notation - submission-20

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-20.py
@@ -0,0 +1,28 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        from math import trunc
+
+        if len(tokens) < 3:
+            return int(tokens[0])
+        
+        division = lambda x, y: trunc(x / y)
+        multi = lambda x, y: x * y
+        substraction = lambda x, y: x - y
+        add = lambda x, y: x + y
+
+        ol_map = {"+": add, "*": multi, "/": division, "-": substraction}
+        ans = 0
+        stack = []
+
+        for i, e in enumerate(tokens):
+            if e not in ol_map.keys():
+                stack.append(int(e))
+            else:
+                if stack:
+                    b = stack.pop(-1)
+                    a = stack.pop(-1)
+
+                    ans = ol_map[e](a, b)
+                    stack.append(ans)
+
+        return stack[-1]
